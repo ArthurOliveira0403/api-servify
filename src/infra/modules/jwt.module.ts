da5d4041f9db15695efbae03ac78ca29bc2da539
ArthurOliveira0403 <arthurviniciusdo@gmail.com>
2026-01-02 19:20:34 -0300

    fix: strategies providers

--- src/infra/modules/jwt.module.ts
+++ src/infra/modules/jwt.module.ts
@@ -3,9 +3,15 @@ import { JWT_SERVICE } from 'src/application/services/jwt.service';
 import { JwtService } from '../jwt/jwt.service';
 import { JwtModule as NestJwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { JwtCompanyStrategy } from '../jwt/strategies/jwt-company-strategy';
+import { JwtAdminStrategy } from '../jwt/strategies/jwt-admin-strategy';
 
 @Module({
-  providers: [{ provide: JWT_SERVICE, useClass: JwtService }],
+  providers: [
+    { provide: JWT_SERVICE, useClass: JwtService },
+    JwtCompanyStrategy,
+    JwtAdminStrategy,
+  ],
   imports: [
     PassportModule,
     NestJwtModule.register({
