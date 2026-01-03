4b71358ca5810d1f4087160ec6a9a030a32a8513
ArthurOliveira0403 <arthurviniciusdo@gmail.com>
2026-01-03 11:54:26 -0300

    feat: add verification (if the service exist) and dto

---
+++ src/application/dtos/delete-service.dto.ts
@@ -0,0 +1,3 @@
+export interface DeleteServiceDTO {
+  serviceId: string;
+}
--- src/application/use-cases/delete-service.use-case.ts
+++ src/application/use-cases/delete-service.use-case.ts
@@ -1,8 +1,9 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import {
   SERVICE_REPOSITORY,
   type ServiceRespository,
 } from 'src/domain/repositories/service.repository';
+import { DeleteServiceDTO } from '../dtos/delete-service.dto';
 
 @Injectable()
 export class DeleteServiceUseCase {
@@ -11,7 +12,10 @@ export class DeleteServiceUseCase {
     private serviceRepository: ServiceRespository,
   ) {}
 
-  async handle(serviceId: string) {
-    await this.serviceRepository.delete(serviceId);
+  async handle(data: DeleteServiceDTO) {
+    const service = await this.serviceRepository.findById(data.serviceId);
+    if (!service) throw new NotFoundException('Service not found');
+
+    await this.serviceRepository.delete(data.serviceId);
   }
 }
